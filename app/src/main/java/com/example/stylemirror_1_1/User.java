391c964112ed57f449dd9fee9d4c036389e39ab3
Areeb Smarty <areebpatel079@gmail.com>
2024-01-18 16:52:32 +0530

    login ui

---
+++ app/src/main/java/com/example/stylemirror_1_1/User.java
@@ -0,0 +1,37 @@
+package com.example.stylemirror_1_1;
+
+public class User {
+    private String userId, username, email;
+
+    public User(String userId, String username, String email) {
+        this.userId = userId;
+        this.username = username;
+        this.email = email;
+    }
+
+
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
