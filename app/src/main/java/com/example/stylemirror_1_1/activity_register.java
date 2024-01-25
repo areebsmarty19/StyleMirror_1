b2bc8e020e07c06691871b4ac03e6287bee86c4b
Areeb Smarty <areebpatel079@gmail.com>
2024-01-25 17:03:24 +0530

    Login Activity

--- app/src/main/java/com/example/stylemirror_1_1/activity_register.java
+++ app/src/main/java/com/example/stylemirror_1_1/activity_register.java
@@ -1,21 +1,20 @@
 package com.example.stylemirror_1_1;
 
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.annotation.SuppressLint;
 import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
-
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.stylemirror_1_1.Activity.MainActivity;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.firestore.auth.User;
 
 import java.util.List;
 import java.util.Objects;
