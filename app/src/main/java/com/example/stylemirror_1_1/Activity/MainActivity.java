bc4be03bc59c6547937f322d54bd62b8d2822118
Dhukka Haris <hdhukka323@gamil.com>
2024-02-29 23:38:09 +0530

    Changed images and do some minor changes.

--- app/src/main/java/com/example/stylemirror_1_1/Activity/MainActivity.java
+++ app/src/main/java/com/example/stylemirror_1_1/Activity/MainActivity.java
@@ -7,11 +7,13 @@ import android.util.Log;
 import android.view.View;
 import android.view.Window;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.content.ContextCompat;
 import androidx.recyclerview.widget.LinearLayoutManager;
 
+import com.etebarian.meowbottomnavigation.MeowBottomNavigation;
 import com.example.stylemirror_1_1.Adapter.PopularAdapter;
 import com.example.stylemirror_1_1.Adapter.SliderAdapter;
 import com.example.stylemirror_1_1.R;
@@ -23,13 +25,21 @@ import com.smarteist.autoimageslider.SliderView;
 
 import java.util.ArrayList;
 
+import kotlin.Unit;
+import kotlin.jvm.functions.Function1;
+
 public class MainActivity extends AppCompatActivity {
 
     ActivityMainBinding binding;
     DatabaseHelper databaseHelper;
 
-    String url1,url2,url3;
-
+//    String url1,url2,url3;
+//    protected final int home = 1;
+//    protected final int book = 2;
+//    protected final int search = 3;
+//    protected final int cart = 4;
+//    protected final int profile = 5;
+//    MeowBottomNavigation meowBottomNavigation;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,38 +48,16 @@ public class MainActivity extends AppCompatActivity {
         setContentView(binding.getRoot());
 
         databaseHelper = new DatabaseHelper(this);
-//        String username = getIntent().getStringExtra("username");
-//        if (username != null ){
-//            binding.textView2.setText(username);
-//        }
+
         if (loggedIn()) {
             binding.textView2.setText(UsernameOfLoggInUser());
         }
-//        if (UsernameOfLoggInUser().equals("")){
-//            binding.textView2.setText(UsernameOfLoggInUser());
+//        else {
+//            String username = getIntent().getStringExtra("username");
+//            binding.textView2.setText(username);
 //        }
-        else {
-            String username = getIntent().getStringExtra("username");
-            binding.textView2.setText(username);
-        }
         statusBarColor();
         initRecyclerView();
-        bottomNavigation();
-
-         binding.explorerBtn.setOnClickListener(new View.OnClickListener() {
-             @Override
-             public void onClick(View v) {
-                 Intent intent = new Intent(MainActivity.this, activity_explorer.class);
-                 startActivity(intent);
-             }
-         });
-         binding.ViewAll.setOnClickListener(new View.OnClickListener() {
-             @Override
-             public void onClick(View v) {
-                 Intent intent = new Intent(MainActivity.this, activity_explorer.class);
-                 startActivity(intent);
-             }
-         });
 
         binding.spec.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -87,14 +75,6 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-//        binding.formals.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//                Intent intent = new Intent(MainActivity.this, activity_shoes_list.class);
-//                startActivity(intent);
-//            }
-//        });
-
         binding.makeup.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -102,6 +82,22 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+
+        binding.ViewAll.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, activity_explorer.class);
+                startActivity(intent);
+            }
+        });
+
+         binding.explorerBtn.setOnClickListener(new View.OnClickListener() {
+             @Override
+             public void onClick(View v) {
+                 Intent intent = new Intent(MainActivity.this, activity_explorer.class);
+                 startActivity(intent);
+             }
+         });
          binding.wishlistBtn.setOnClickListener(new View.OnClickListener() {
              @Override
              public void onClick(View v) {
@@ -109,18 +105,24 @@ public class MainActivity extends AppCompatActivity {
                  startActivity(intent);
              }
          });
-         binding.profileBtn.setOnClickListener(new View.OnClickListener() {
-             @Override
-             public void onClick(View v) {
-                 Intent intent = new Intent(MainActivity.this, activity_profile.class);
-                 startActivity(intent);
-             }
-         });
-
          binding.searchBtn.setOnClickListener(new View.OnClickListener() {
              @Override
              public void onClick(View v) {
                  Intent intent = new Intent(MainActivity.this, activity_search.class);
+                 startActivity(intent);
+             }
+         });
+         binding.cartBtn.setOnClickListener(new View.OnClickListener() {
+             @Override
+             public void onClick(View v) {
+                 Intent intent = new Intent(MainActivity.this, CartActivity.class);
+                 startActivity(intent);
+             }
+         });
+         binding.profileBtn.setOnClickListener(new View.OnClickListener() {
+             @Override
+             public void onClick(View v) {
+                 Intent intent = new Intent(MainActivity.this, activity_profile.class);
                  startActivity(intent);
              }
          });
@@ -158,19 +160,65 @@ public class MainActivity extends AppCompatActivity {
 
         // to start autocycle below method is used.
         sliderView.startAutoCycle();
+
+//        meowBottomNavigation = findViewById(R.id.appBar);
+//        meowBottomNavigation.add(new MeowBottomNavigation.Model(home,R.drawable.baseline_view_list_24));
+//        meowBottomNavigation.add(new MeowBottomNavigation.Model(book,R.drawable.baseline_bookmark_border_24));
+//        meowBottomNavigation.add(new MeowBottomNavigation.Model(search,R.drawable.baseline_search_24));
+//        meowBottomNavigation.add(new MeowBottomNavigation.Model(cart,R.drawable.baseline_shopping_cart_24));
+//        meowBottomNavigation.add(new MeowBottomNavigation.Model(profile,R.drawable.baseline_person_outline_24));
+//
+//        meowBottomNavigation.setOnClickMenuListener(new Function1<MeowBottomNavigation.Model, Unit>() {
+//            @Override
+//            public Unit invoke(MeowBottomNavigation.Model model) {
+//                String name;
+//                switch (model.getId()){
+//                    case home:
+//                        name="Home";
+//                        Toast.makeText(MainActivity.this, name, Toast.LENGTH_SHORT).show();
+//                        Intent intent1 = new Intent(MainActivity.this,activity_explorer.class);
+//                        startActivity(intent1);
+//                        break;
+//
+//                    case book:
+//                        name="WishList";
+//                        Toast.makeText(MainActivity.this, name, Toast.LENGTH_SHORT).show();
+//                        Intent intent2 = new Intent(MainActivity.this,activity_fav_items.class);
+//                        startActivity(intent2);
+//                        break;
+//
+//                    case search:
+//                        name="Search";
+//                        Toast.makeText(MainActivity.this, name, Toast.LENGTH_SHORT).show();
+//                        Intent intent3 = new Intent(MainActivity.this,activity_search.class);
+//                        startActivity(intent3);
+//                        break;
+//
+//                    case cart:
+//                        name="Cart";
+//                        Toast.makeText(MainActivity.this, name, Toast.LENGTH_SHORT).show();
+//                        Intent intent4 = new Intent(MainActivity.this,CartActivity.class);
+//                        startActivity(intent4);
+//                        break;
+//
+//                    case profile:
+//                        name="Profile";
+//                        Toast.makeText(MainActivity.this, name, Toast.LENGTH_SHORT).show();
+//                        Intent intent5 = new Intent(MainActivity.this,activity_profile.class);
+//                        startActivity(intent5);
+//                        break;
+//                }
+//                return null;
+//            }
+//        });
     }
 
     private String UsernameOfLoggInUser(){
         return databaseHelper.getUsernameByIfLoggIn();
     }
-//    private String EmailOfLoggInUser(){
-//        return databaseHelper.getEmailByIfLoggIn();
-//    }
+
     private boolean loggedIn() {
         return databaseHelper.isLoggedIn(); // You need to implement isLoggedIn method in DatabaseHelper
-    }
-    private void bottomNavigation() {
-        binding.cartBtn.setOnClickListener(v -> startActivity(new Intent(MainActivity.this, CartActivity.class)));
     }
 
     private void statusBarColor() {
@@ -182,17 +230,7 @@ public class MainActivity extends AppCompatActivity {
         ArrayList<PopularDomain> items=new ArrayList<>();
         items.add(new PopularDomain(1,"Air Max 270 Sneakers For Men","shoess_1",0,4,9796,"Wipe with clean cloth Nike's first lifestyle Air Max brings you style, comfort and big attitude in the Nike Air Max 270. The design draws inspiration from Air Max icons, showcasing Nike's greatest innovation with its large window and fresh array of colours."));
         items.add(new PopularDomain(16,"UV Protection Aviator Sunglasses","spects_7",0,0,1274,"Material Composition"));
-        items.add(new PopularDomain(3,"Phone","item_3",3,4.9,800,"Immerse yourself in a world of vibrant visuals and\n" +
-                " immersive sound with the monitor.\n" +
-                " Its cutting-edge monitor technology brings every\n" +
-                " scene to life with striking clarity and rich colors.\n" +
-                " With seamless integration and a sleek, modern\n" +
-                " design, the monitor Pro is not just a monitor , but a\n" +
-                " centerpiece for your entertainment space.\n" +
-                "With its sleek, modern design, the monitor is\n" +
-                " not just a TV, but a centerpiece for your \n" +
-                "entertainment space. The ultra-slim bezel and\n" +
-                " premium finish blend seamlessly with any decor"));
+        items.add(new PopularDomain(3,"Phone","item_3",3,4.9,800,"Immerse yourself in a world of vibrant visuals and immersive sound with the monitor Its cutting-edge monitor technology brings every scene to life with striking clarity and rich colors With seamless integration and a sleek, modern design, the monitor Pro is not just a monitor , but a centerpiece for your entertainment space.With its sleek, modern design, the monitor is not just a TV, but a centerpiece for your entertainment space. The ultra-slim bezel and premium finish blend seamlessly with any decor"));
 
         binding.PopularView.setLayoutManager(new LinearLayoutManager(this,LinearLayoutManager.VERTICAL,false));
         binding.PopularView.setAdapter(new PopularAdapter(items,this));
